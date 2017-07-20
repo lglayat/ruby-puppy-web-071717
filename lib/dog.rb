46272703d628f814d9df9fb2ee751d6a97731eff
lglayat <glayatle@g.cofc.edu>
2017-07-20 15:13:33 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  @@all = []
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each do |doge|
+      puts doge.name
+    end
+  end
+
+
+
+
+end
